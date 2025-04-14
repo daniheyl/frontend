f678f2e80c1a4d10634aeb6e918a4b924b2bccfd
daniheyl <35920095+daniheyl@users.noreply.github.com>
2025-04-14 10:13:06 +0200

    added fireship hosting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,14 @@
 // src/app/app.config.ts
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withFetch()),
     provideRouter(routes),
     provideHttpClient(withInterceptors([AuthInterceptor]))
   ]
